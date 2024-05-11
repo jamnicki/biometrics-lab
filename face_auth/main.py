b52f1f71d2547263e6bef7a13ae4ca98b5d8b9f1
Jedrzej Jamnicki <jedrzej.jamnicki@gmail.com>
2024-05-12 00:13:43 +0200

    refactor: p2 prawei gotowe ez

---
+++ face_auth/main.py
@@ -0,0 +1,72 @@
+import streamlit as st
+from PIL import Image
+import os
+import cv2
+from users_db import UsersDB
+from pathlib import Path
+
+# MODEL = "VGG-Face"
+# USER_DB_PATH = Path("./data/user_db.pkl")
+MODEL = "Facenet"
+USER_DB_PATH = Path("./data/user_db_facenet.pkl")
+
+
+
+# Funkcja do tworzenia nowego profilu
+def create_profile(db, login):
+    if db.add_record(login, "tmp/uploaded_registry_image.jpg"):
+        st.success(f"Nowy profil dla {login} został utworzony!")
+        return db
+    else:
+        st.error(f"Nowy profil dla {login} nie został utworzony!")
+        return db
+
+# Funkcja do autoryzacji
+def authenticate(db, login):
+    print(len(db.data))
+    result = db.verify_user("tmp/uploaded_auth_image.jpg", login)
+    if result[1]:
+        print(f"Similarity {result[0]}")
+        st.success(f"Witaj {login}!  Cos_dist = {result[0]:.4}")
+        return db
+    else:
+        print(f"Similarity {result[0]}")
+        st.error(f"Błąd autoryzacji dla użytkownika {login}! Cos_dist = {result[0]:.4}")
+        return db
+    
+
+def main():
+    db = UsersDB(model=MODEL)
+    db.load_db(USER_DB_PATH)
+    st.title("Aplikacja do autoryzacji za pomocą zdjęć twarzy")
+
+    # Wybór akcji: Tworzenie profilu vs. Autoryzacja
+    action = st.radio("Wybierz akcję:", ("Stwórz nowy profil", "Autoryzuj"))
+
+    if action == "Stwórz nowy profil":
+        st.header("Stwórz nowy profil")
+        name = st.text_input("Podaj login:")
+        image = st.file_uploader("Wgraj zdjęcie:", type=["jpg", "png", "jpeg"])
+        
+        if name and image:
+            st.image(image, use_column_width=True)
+            with open(os.path.join('tmp', 'uploaded_registry_image.jpg'), 'wb') as f:
+                f.write(image.read())
+            db = create_profile(db, name)
+            db.save_db(USER_DB_PATH)
+            os.remove(os.path.join('tmp', 'uploaded_registry_image.jpg'))
+            print(len(db.data))
+
+    elif action == "Autoryzuj":
+        st.header("Autoryzacja")
+        name = st.text_input("Podaj login:")
+        image_auth = st.file_uploader("Wgraj zdjęcie:", type=["jpg", "png", "jpeg"])
+        if name and image_auth:
+            st.image(image_auth, use_column_width=True)
+            with open(os.path.join('tmp', 'uploaded_auth_image.jpg'), 'wb') as f:
+                f.write(image_auth.read())
+            db = authenticate(db, name)
+            os.remove(os.path.join('tmp', 'uploaded_auth_image.jpg'))
+
+if __name__ == "__main__":
+    main()
---
+++ face_auth/users_db.py
@@ -0,0 +1,66 @@
+import json
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from deepface import DeepFace
+
+
+class UsersDB:
+    def __init__(self, model):
+        # Tworzenie pustego DataFrame
+        self.model = model
+        self.data = pd.DataFrame(columns=["id", "face_repr"])
+
+        with open("./data/exp_img_embeddings.json", "rb") as f:
+            self.cached_embeddings = json.load(f)
+
+    def CosDist(self, source_representation, test_representation):
+        a = np.matmul(np.transpose(source_representation), test_representation)
+        b = np.sum(np.multiply(source_representation, source_representation))
+        c = np.sum(np.multiply(test_representation, test_representation))
+        return 1 - (a / (np.sqrt(b) * np.sqrt(c)))
+
+    def add_record(self, id_, img_path):
+        # Dodawanie nowego rekordu do DataFrame
+        if id_ in self.data['id'].astype('str').values:
+            print("Osoba już znajduje się w bazie danych!")
+            return False
+        pred = DeepFace.represent(
+            img_path=img_path, model_name=self.model, enforce_detection=False
+        )
+        if pred is None:
+            print("Twarz nie wykryta!")
+            return False
+
+        face_repr = pred[0]["embedding"]
+        new_row = pd.DataFrame({"id": [id_], "face_repr": [face_repr]})
+        self.data = pd.concat([self.data, new_row], ignore_index=True)
+        print(f"Pomyślnie dodano nową osobe {id_=} :) ")
+        return True
+
+    def verify_user(self, img_path, identity, threshold = 0.3, cache=False):
+        if str(identity) not in self.data["id"].astype('str').values:
+                print(f"Nie ma takiej osoby w bazie danych {identity=}")
+                return None, False
+        if cache:
+            input_face_repr = self.cached_embeddings[self.model][str(img_path)]
+        else:
+            input_face_repr = self.get_img_embedding(img_path)
+        target_identity_face_repr = self.data[self.data["id"].astype('str') == str(identity)][
+            "face_repr"
+        ].item()
+        cos_dist = self.CosDist(input_face_repr, target_identity_face_repr)
+        authorized = cos_dist < threshold
+        return cos_dist, authorized
+
+    def get_img_embedding(self, img_path):
+        return DeepFace.represent(
+            img_path=img_path, model_name=self.model, enforce_detection=False
+        )[0]["embedding"]
+
+    def save_db(self, path):
+        self.data.to_pickle(path)
+
+    def load_db(self, path):
+        self.data = pd.read_pickle(path)
