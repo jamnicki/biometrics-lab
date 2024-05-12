7d50f131c4acb677a96486d2f78b6b26e3fcd12f
Jedrzej Jamnicki <jedrzej.jamnicki@gmail.com>
2024-05-12 10:04:39 +0200

    feat: eval df1

--- speech_auth/speech_users_db.py
+++ speech_auth/speech_users_db.py
@@ -8,12 +8,11 @@ import wespeaker
 
 class SpeechUsersDB:
     def __init__(self, lang: str = "english"):
-        # Tworzenie pustego DataFrame
         self.model = wespeaker.load_model(lang)
         self.data = pd.DataFrame(columns=["id", "speech_repr"])
 
-        # with open("./data/speech/exp_audio_embeddings.json", "rb") as f:
-        #     self.cached_embeddings = json.load(f)
+        with open("../data/speech/exp_speech_embeddings.json", "rb") as f:
+            self.cached_embeddings = json.load(f)
 
     def CosDist(self, source_representation, test_representation):
         a = np.matmul(np.transpose(source_representation), test_representation)
@@ -28,12 +27,16 @@ class SpeechUsersDB:
             print("Błąd podczas wykrywania głosu!")
             print(e)
 
-    def add_record(self, id_, audio_fpath):
+    def add_record(self, id_, audio_fpath, cache=False):
         # Dodawanie nowego rekordu do DataFrame
         if id_ in self.data['id'].astype('str').values:
             print("Osoba już znajduje się w bazie danych!")
             return False
-        speech_repr = self.get_embedding(audio_fpath)
+        if cache:
+            speech_repr = self.cached_embeddings[str(audio_fpath)]
+        else:
+            speech_repr = self.get_embedding(audio_fpath)
+
         new_row = pd.DataFrame({"id": [id_], "speech_repr": [speech_repr]})
         self.data = pd.concat([self.data, new_row], ignore_index=True)
         print(f"Pomyślnie dodano nową osobe {id_=} :) ")
